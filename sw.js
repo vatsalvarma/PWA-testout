dc7abfa3d58c06d99f857140c68489beee7d4d58
Vatsal varma <144918256+vatsalvarma@users.noreply.github.com>
2026-05-14 15:51:02 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// sw.js
+
+const CACHE_NAME = "parallax-pwa-v1"
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+]
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+      })
+  )
+
+})
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request)
+      })
+  )
+
+})
